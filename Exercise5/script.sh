d34560e8892662e04acfa0ca5f70f98e8774c6cf
gabrielefronze <sucre.91@hotmail.it>
2020-02-12 00:13:14 +0100

    Improving Ex5

--- Exercise5/script.sh
+++ Exercise5/script.sh
@@ -3,4 +3,6 @@
 echo "This is my test script"
 uname -a
 echo "This is your custom argument" $1
+echo "Creating a little surprise"
+echo "Roma non far la stupida stasera" > surprise.txt
 echo "Bye bye"
