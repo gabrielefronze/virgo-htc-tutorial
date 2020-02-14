c08b5c143a82ef4d4cd9b54b1c0899b0650ce69b
gabrielefronze <sucre.91@hotmail.it>
2020-02-14 11:50:42 +0100

    Using my homedir

--- Hands-on/CW/wrapper-cw-cvmfs.sh
+++ Hands-on/CW/wrapper-cw-cvmfs.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-/cvmfs/virgo.ego-gw.it/tests/test_cw/run_HFDF.sh /cvmfs/virgo.ego-gw.it/tests/test_cw/v90 /storage/gpfs_virgo4/virgo4/virgo/RomePSS/H/hfdf/O2/C02_NewAllRun/256Hz/input/2018-04-16/ O2LH_01_0127 067_-81.305_-90.000 /storage/gpfs_virgo3/home/aeinstein/ 0.99 20 0 -1.0*10^-8 -2.0*10^-9 0 0
+/cvmfs/virgo.ego-gw.it/tests/test_cw/run_HFDF.sh /cvmfs/virgo.ego-gw.it/tests/test_cw/v90 /storage/gpfs_virgo4/virgo4/virgo/RomePSS/H/hfdf/O2/C02_NewAllRun/256Hz/input/2018-04-16/ O2LH_01_0127 067_-81.305_-90.000 /storage/gpfs_virgo3/home/gfronze/ 0.99 20 0 -1.0*10^-8 -2.0*10^-9 0 0
